ed1a6599caf1f008f46becac64ff296487a1bfc4
Pureshwar <pureshwargonekar7@gmail.com>
2023-12-09 20:42:01 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const cors=require("cors");
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 require('./src/db/conn');
 const cookieParser = require('cookie-parser');
@@ -13,6 +13,19 @@ app.use(cookieParser());
 const studentRoutes = require("./routes/ProfileRoute");
 app.use("/", studentRoutes);
 
+const allowedOrigins = ['https://erpapp-frontend.vercel.app', "https://erp.thejbschool.com"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
     console.log("Backend server running on port " + PORT)
