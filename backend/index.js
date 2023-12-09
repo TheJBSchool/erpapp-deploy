9dfc272e32dd31199a9d0c6366aa444425f4364c
Pureshwar <pureshwargonekar7@gmail.com>
2023-12-09 21:41:02 +0530

    test 4

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,15 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const cors=require("cors");
-app.use(cors());
+// app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
+
 app.use(express.json());
 require('./src/db/conn');
 const cookieParser = require('cookie-parser');
